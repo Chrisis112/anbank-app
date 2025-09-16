c93df58ded05a456941d297f18d96c29167059e6
Андрей Коваленко <mr.chrisis112@gmail.com>
2025-09-16 18:03:30 +0300

    Добавил phantom encryption и что бы оплата проходила на телефоне101

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,63 +8,12 @@ import '../styles/components.css';
 import '@solana/wallet-adapter-react-ui/styles.css';
 
 import React from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
+
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
+import { WalletContextProvider } from '@/components/providers/WallletProvider';
 
-import {
-  SolanaMobileWalletAdapter,
-  createDefaultAddressSelector,
-  AuthorizationResultCache,
-} from '@solana-mobile/wallet-adapter-mobile';
-
-class SimpleAuthorizationResultCache implements AuthorizationResultCache {
-  async get() {
-    return null;
-  }
-  async set() {}
-  async delete() {}
-  async clear() {}
-}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const network =
-    process.env.NEXT_PUBLIC_SOLANA_NETWORK === 'https://api.mainnet-beta.solana.com'
-      ? WalletAdapterNetwork.Mainnet
-      : WalletAdapterNetwork.Devnet;
-
-  const endpoint = process.env.NEXT_PUBLIC_SOLANA_NETWORK || clusterApiUrl(network);
-
-  const mobileWalletAdapter = React.useMemo(() => {
-    return new SolanaMobileWalletAdapter({
-      appIdentity: {
-        name: 'CryptoChat',
-        uri: typeof window !== 'undefined' ? window.location.origin : 'https://yourdomain.com',
-      },
-      authorizationResultCache: new SimpleAuthorizationResultCache(),
-      addressSelector: createDefaultAddressSelector(),
-      chain: network === WalletAdapterNetwork.Mainnet ? 'mainnet-beta' : 'devnet',
-      onWalletNotFound: async () => {
-        alert('Solana Mobile Wallet не найден! Пожалуйста, установите его.');
-      },
-    });
-  }, [network]);
-
-  const wallets = React.useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter({ network }),
-      new TorusWalletAdapter(),
-      mobileWalletAdapter, // добавлен адаптер для мобильных
-    ],
-    [network, mobileWalletAdapter]
-  );
 
   return (
     <html lang="en">
@@ -86,11 +35,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         />
       </head>
       <body>
-        <ConnectionProvider endpoint={endpoint}>
-          <WalletProvider wallets={wallets} autoConnect>
-            <WalletModalProvider>{children}</WalletModalProvider>
-          </WalletProvider>
-        </ConnectionProvider>
+            <WalletContextProvider>{children}</WalletContextProvider>
       </body>
     </html>
   );
