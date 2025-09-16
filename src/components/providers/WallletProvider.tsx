b1676cdf0438143905a6bb179269247ea0998ee1
Андрей Коваленко <mr.chrisis112@gmail.com>
2025-09-16 18:08:29 +0300

    Добавил phantom encryption и что бы оплата проходила на телефоне102

--- src/components/providers/WallletProvider.tsx
+++ src/components/providers/WallletProvider.tsx
@@ -6,8 +6,6 @@ import {
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
   PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  TorusWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
@@ -59,9 +57,6 @@ export const WalletContextProvider: FC<Props> = ({ children }) => {
   const wallets = useMemo(
     () => [
       new PhantomWalletAdapter(),
-      new SolflareWalletAdapter({ network }),
-      new TorusWalletAdapter(),
-      mobileWalletAdapter,
     ],
     [network, mobileWalletAdapter]
   );
