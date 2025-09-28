96e695a1f8f4b05dff4762dba417d05a33898ae8
Андрей Коваленко <mr.chrisis112@gmail.com>
2025-09-28 19:51:49 +0300

    добавил push уведомления34

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
